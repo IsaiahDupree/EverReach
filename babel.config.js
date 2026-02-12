4a93ed2e099c279357001aa10c278d47720b8421
Isaiah Dupree <Isaiahdupree33@gmail.com>
2026-02-11 21:49:43 -0500

    fix: scope dynamic-import babel plugin to test env only (fixes Metro crash)

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
+
+  const plugins = [
+    // NOTE: react-native-reanimated/plugin MUST be last
+    'react-native-reanimated/plugin',
+  ];
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Convert dynamic import() to require() so Jest can intercept with mocks
-      '@babel/plugin-transform-dynamic-import',
-      // NOTE: react-native-reanimated/plugin MUST be last
-      'react-native-reanimated/plugin',
-    ],
+    plugins,
+    env: {
+      test: {
+        plugins: [
+          // Convert dynamic import() to require() so Jest can intercept with mocks
+          '@babel/plugin-transform-dynamic-import',
+        ],
+      },
+    },
   };
 };
